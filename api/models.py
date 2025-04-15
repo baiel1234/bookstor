b9ce10a1ba16484e3d154bad3fd25dbe0193ff59
baiel1234 <avtandilovbajel47@gmail.com>
2025-04-15 14:44:37 +0600

    add contact,price,description

--- api/models.py
+++ api/models.py
@@ -8,7 +8,10 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
-    image = models.ImageField(upload_to='book_images/', null=True, blank=True)  # <- новое поле
+    image = models.ImageField(upload_to='book_images/', null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    contact = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
--- api/serializers.py
+++ api/serializers.py
@@ -29,15 +29,18 @@ class OwnerSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    owner = OwnerSerializer(read_only=True)  # <- при GET отдаём объект
+    owner = OwnerSerializer(read_only=True)
     owner_id = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.all(), write_only=True, source='owner'
-    )  # <- при POST/PUT принимаем id
+    )
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'owner', 'owner_id', 'image']
-
+        fields = [
+            'id', 'title', 'author', 'owner', 'owner_id', 'image',
+            'price', 'contact', 'description'
+        ]
+        
 class UserUpdateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
 
